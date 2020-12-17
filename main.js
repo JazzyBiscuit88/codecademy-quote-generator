66d9dc581f5a6d0a552004bcc523bd3c14486da0
Shawn Wilson <shawnwilsontrpt@gmail.com>
2020-12-17 10:15:58 -0500

    added date as header

---
+++ main.js
@@ -0,0 +1,11 @@
+
+// generate random number
+// quotes are tied to a number
+
+// could use a quote object
+
+console.log('---------------');
+const date = new Date();
+console.log(date.toDateString());
+console.log('---------------');
+
